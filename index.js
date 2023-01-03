626d57792213fc83a0166988820c584cf75bba97
johnhongannguyen <johhaneshongan@gmail.com>
2023-01-03 05:35:09 -0800

    extracting routes

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);const express = require('express');
 const app = express();
-const genres = require('./routes/genres');
 const mongoose = require('mongoose');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users')
-const auth = require('./routes/auth');
 const config = require('config');
-const error = require('./middleware/error')
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb')
+require('./startup/route')(app);
+
 
 // uncaught exception
 // process.on('uncaughtException', ex =>{
@@ -45,16 +40,7 @@ mongoose.connect('mongodb://localhost/genres',{useNewUrlParser: true,useUnifiedT
     .catch(err => console.log('Cannot connect to MongoDB'))
 
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers',customers);
-app.use('/api/movies',movies);
-app.use('/api/rentals',rentals);
-app.use('/api/users', users);
-app.use('/api/auth',auth);
 
-
-app.use(error)
 
 app.get('/',(req,res)=>{
   res.send('Welcome to genres API')
